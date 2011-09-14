0962caaed3fbfcca29ca65c2a42e17c07ab6257a
Matt Hisamoto <mwhisamoto@gmail.com>
2011-09-14 15:45:41 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BotbVoting::Application.config.secret_token = '8918e92d36630d66b2ae82993e9b7541f81185cfdeb01f5939cf1ab4141185ea123c87d5592dd1c5c0dc22a4899bf14025567fce76e7ce3488d7aaaa8732c381'
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+BotbVoting::Application.routes.draw do
+  
+  #resources :votes, collection => {:status => :get}
+  #resources :sms_votes
+   
+end
