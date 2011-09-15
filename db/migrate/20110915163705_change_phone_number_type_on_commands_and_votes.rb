ac8b77d0f4e146d42df153c755e8a115bbaacaec
Matt Hisamoto <mwhisamoto@gmail.com>
2011-09-15 12:41:44 -0400

    change type of phone number

--- db/migrate/20110915163705_change_phone_number_type_on_commands_and_votes.rb
+++ db/migrate/20110915163705_change_phone_number_type_on_commands_and_votes.rb
@@ -1,21 +1,11 @@
 class ChangePhoneNumberTypeOnCommandsAndVotes < ActiveRecord::Migration
-  def up
+  def change
     
     remove_column :votes, :phone_number
     remove_column :commands, :phone_number
     
     add_column :votes, :phone_number, :string
     add_column :commands, :phone_number, :string
-    
-  end
-
-  def down
-    
-    remove_column :votes, :phone_number
-    remove_column :commands, :phone_number
-    
-    add_column :votes, :phone_number, :integer
-    add_column :commands, :phone_number, :integer
     
   end
 end
